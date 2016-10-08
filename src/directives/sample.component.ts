84c63eef762baaf22560be6e9ab58ad94be212a9
Braulio Lomelí <brauliolomeli@gmail.com>
2016-10-08 13:09:56 -0500

    Add build script and dist to repo

--- src/directives/sample.component.ts
+++ src/directives/sample.component.ts
@@ -2,9 +2,7 @@ import {Component} from '@angular/core';
 
 @Component({
   selector: 'sample-component',
-  template: `
-    Sample component
-  `
+  templateUrl: 'sample.component.html'
 })
 export class SampleComponent {
 
