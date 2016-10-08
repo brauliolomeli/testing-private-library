a3fb782604f1bbc5d7d17c67661d4ac569cbc08f
Braulio Lomelí <brauliolomeli@gmail.com>
2016-10-08 14:23:34 -0500

    Build app

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,1 +1,9 @@
+import { DoorBuilderModule } from './door-builder.module';
+import { SelectDoorComponent } from './select-door/select-door.component';
 export * from './door-builder.module';
+export * from './select-door/select-door.component';
+declare var _default: {
+    DoorBuilderModule: typeof DoorBuilderModule;
+    components: typeof SelectDoorComponent[];
+};
+export default _default;
--- dist/index.js
+++ dist/index.js
@@ -2,5 +2,15 @@
 function __export(m) {
     for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
 }
+var door_builder_module_1 = require('./door-builder.module');
+var select_door_component_1 = require('./select-door/select-door.component');
 __export(require('./door-builder.module'));
+__export(require('./select-door/select-door.component'));
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = {
+    DoorBuilderModule: door_builder_module_1.DoorBuilderModule,
+    components: [
+        select_door_component_1.SelectDoorComponent
+    ]
+};
 //# sourceMappingURL=index.js.map
