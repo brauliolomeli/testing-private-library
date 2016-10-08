67c7f02b626ef8365db166eee3ca088459b8ece3
Braulio Lomelí <brauliolomeli@gmail.com>
2016-10-08 14:20:51 -0500

    Organize index.ts file

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,12 @@
+import { DoorBuilderModule } from './door-builder.module';
+import { SelectDoorComponent } from './select-door/select-door.component';
+
 export * from './door-builder.module';
+export * from './select-door/select-door.component';
+
+export default {
+  DoorBuilderModule : DoorBuilderModule,
+  components: [
+    SelectDoorComponent
+  ]
+};
